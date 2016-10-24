e42dc2620e8d7d82b6497a28bee15d7ed5baf065
Dani van der Werf <danivdwerf@Danis-MacBook-Pro.local>
2016-10-24 09:38:27 +0200

    fuck my butt

---
+++ Assets/_Scripts/ChangeImage.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeImage : MonoBehaviour
+{
+	[SerializeField] private Sprite[] images; 
+	[SerializeField] private Image display;
+	private int i;
+	private void Start()
+	{
+		i = 0;
+		display.sprite = images [i];
+
+	}
+	public void NextImage ()
+	{
+		i++;
+		if (i > images.Length - 1)
+		{
+			i = 0;
+		}
+		display.sprite = images [i];
+	}
+
+	public void PreviousImage()
+	{
+		i--;
+		if(i<0)
+		{
+			i=images.Length-1;
+		}
+		display.sprite = images[i];
+	}
+}
---
+++ Assets/_Scripts/HandleInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HandleInput : MonoBehaviour 
+{
+	[SerializeField] private Button leftButton;
+	[SerializeField] private Button rightButton;
+	private ChangeImage changeImage;
+
+	private void Start()
+	{
+		changeImage = GetComponent<ChangeImage> ();
+		leftButton.onClick.AddListener (delegate(){changeImage.PreviousImage();});
+		rightButton.onClick.AddListener (delegate(){changeImage.NextImage();});
+	}
+}
